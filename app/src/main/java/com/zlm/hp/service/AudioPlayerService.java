cef3c7d8ec048414ccbeb383e5388865586fc78a
zhangliangming <316257874@qq.com>
2018-01-16 16:32:09 +0800

    修复通知栏图标问题

--- app/src/main/java/com/zlm/hp/service/AudioPlayerService.java
+++ app/src/main/java/com/zlm/hp/service/AudioPlayerService.java
@@ -165,7 +165,6 @@ public class AudioPlayerService extends Service {
 
         mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        int icon = R.mipmap.singer_def;
         String tickerText = "乐乐音乐";
 
         //判断系统版本
@@ -187,7 +186,7 @@ public class AudioPlayerService extends Service {
             mPlayBarNotification = new Notification.Builder(getApplicationContext())
                     .setContentTitle(tickerText)
                     .setContentText("开心每一天")
-                    .setSmallIcon(icon)
+                    .setSmallIcon(R.mipmap.notifi_icon)
                     .setChannelId(CHANNEL_ID)
                     .build();
         } else {
@@ -195,7 +194,7 @@ public class AudioPlayerService extends Service {
             mPlayBarNotification = new Notification.Builder(getApplicationContext())
                     .setContentTitle(tickerText)
                     .setContentText("开心每一天")
-                    .setSmallIcon(icon)
+                    .setSmallIcon(R.mipmap.singer_def)
                     .build();
         }
 
@@ -433,8 +432,9 @@ public class AudioPlayerService extends Service {
         mAudioBroadcastReceiver.unregisterReceiver(getApplicationContext());
         mNotificationReceiver.unregisterReceiver(getApplicationContext());
 
-        //
-        stopForeground(true);
+        //关闭通知栏
+        mNotificationManager.cancel(mNotificationPlayBarId);
+
         releasePlayer();
         logger.i("音频播放服务销毁");
         super.onDestroy();
